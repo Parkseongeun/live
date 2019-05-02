bca6f93d1533d38ca23bd5a41ec1db7d2f6822c4
hivelab948 <hivelab948@gepplus.co.kr>
2019-05-02 16:48:52 +0900

    bitcoin

--- bitcoin/src/App.js
+++ bitcoin/src/App.js
@@ -1,29 +1,9 @@
-import React, { Component } from 'react';
-import './css/App.css';
-import Crypto from './components/Crypto';
-
+import React, { Component } from "react";
+import Crypto from "./components/Crypto";
 
 class App extends Component {
-
   render() {
-    return (
-      <div className="App">
-        <header>
-          <h1>Live Cryptocurrency</h1>
-          <p className="countdown">60</p>
-          <button type="button" className="button_reload"><span className="for-a11y">Refresh</span></button>
-        </header>
-
-        <div className="App_coins">
-          <h2>Live Prices <span><span></span>12:20</span></h2>
-          <Crypto />
-        </div>
-
-        {/* <div className="App_news">
-          {this.state.newsList ? this._rendernewsList() : "Loading" }
-        </div> */}
-      </div>
-    )
+    return <Crypto />;
   }
 }
 
--- bitcoin/src/components/Crypto.js
+++ bitcoin/src/components/Crypto.js
@@ -1,92 +1,64 @@
-import React, {Component} from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
-/* eslint-disable no-unused-expressions */
+const dataRealCrypto = "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH&tsyms=KRW&api_key=9e03a6c81111584d72793303ce56788f3301c6cd9a3ed36484e1c0703d32385e";
+// http://jsonpathfinder.com
+// https://github.com/axios/axios
+
 class Crypto extends Component {
-
   constructor(props) {
-    super(props); //부모클래스 super
+    super(props);
     this.state = {
-      livePrice: {
-          BTC: false,
-          ETH: false,
-          XRP: false
-      },
-      liveRatio: {
-          BTC: false,
-          ETH: false,
-          XRP: false
+      realCrypto: {
+        BTC : 0,
+        ETH : 0
       }
     }
   }
-
-  componentDidMount() {
-    this._callApi();
-    this.interval = setInterval(() => this._callApi(), 60 * 1000);
+  
+  componentDidMount(){
+    this.funcCrypto();
   }
 
-  _getApi = async () => {
-    const currency = await this._callApi()
-    this.setState({
-      currency
-    })
-  }
-  _callApi = () => {
-    axios
-    .get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,XRP&tsyms=KRW&api_key=9e03a6c81111584d72793303ce56788f3301c6cd9a3ed36484e1c0703d32385e')
-    .then(json => console.log(json))
+  funcCrypto = async () => {
+    axios.get(dataRealCrypto)
     .then(response => {
-      const result = response.data.filter(currency =>
-        wanted.includes(currency.id),
-      )
-      this.setState = {
-        livePrice: {
-          BTC: price.BTC,
-          ETH: price.ETH,
-          XRP: price.XRP
-        },
-        liveRatio: {
-          BTC: price.BTC,
-          ETH: price.ETH,
-          XRP: price.XRP
-        }
+      const obj = Object.keys(this.realCrypto)
+      for (var i = 0; i < obj.length; i++) {
+      this.realCrypto[obj[i]] =
+        response.data.DISPLAY[obj[i]].KRW.PRICE
       }
-    }).catch(err => console.log(err))
-   }
+      this.setState({
+        realCrypto: {
+          BTC : PRICE.BTC,
+          ETH : PRICE.ETH
+        }
+      })
+    })
+    .catch(err => console.log(err))
+  }
 
+  // fetch(dataRealCrypto)
+  // .then(response => response.json())
+  // .then(json => console.log(json))
+  // .catch(err => console.log(err));
 
-  render() { 
-    const Currency = props => {
-      const {
-        name,
-        symbol,
-        price_usd,
-        percent_change_1h,
-        percent_change_24h,
-        percent_change_7d,
-      } = props.data
-      return (
-        <li className="currency">
-          <p className="currency-name">
-            {name} ({symbol})
-          </p>
-          <h1>${(+price_usd).toFixed(2)}</h1>
-          <p>{percent_change_1h}% 1hr</p>
-          <p>{percent_change_24h}% 24hrs</p>
-          <p>{percent_change_7d}% 7days</p>
-        </li>
-      )
+  render() {
+    const { realCrypto } = this.state;
+    console.log(dataRealCrypto)
+    if (realCrypto.length > 0) {
+      return realCrypto.map(realCrypto => {
+        return (
+          <div>
+            <p>BTC : {realCrypto.BTC}</p>
+            <p>ETH : {realCrypto.ETH}</p>
+          </div>
+        );
+      });
+    } else {
+      return 'error';
     }
-    
-    let crypto = this.state.data.map(currency => ( // map() 업데이트해야할 항목을 추적
-      <Currency data={currency} key={currency.id} />
-    ))
-    return (
-      <div className="crypto-container">
-        <ul className="crypto">{crypto}</ul>
-      </div>
-    )
   }
 }
 
-export default Crypto
+export default Crypto;
